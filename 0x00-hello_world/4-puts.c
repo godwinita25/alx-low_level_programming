064c00b154ec6a98654065a993bdb7d30271c071
godwinita25 <godwinita05@gmail.com>
2023-02-16 17:07:36 -0800

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,6 @@
 #include <stdio.h>
-/** main - Entry we'r to write a c program that prints a line with put function without using the printf
+/**
+* main - Entry write a c program that prints a line with put function
 * Return: Always 0
 */
 int main(void)
