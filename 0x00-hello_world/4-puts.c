e4975cd07c730c01911c72a19495e72506c9d20a
godwinita25 <godwinita05@gmail.com>
2023-02-16 17:03:44 -0800

    4-puts

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+/** main - Entry we'r to write a c program that prints a line with put function without using the printf
+* Return: Always 0
+*/
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
