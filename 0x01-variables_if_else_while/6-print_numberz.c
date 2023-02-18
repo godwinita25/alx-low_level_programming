42cc4cfe9c7734dd6d08009cd093b3e5f0d626d1
godwinita25 <godwinita05@gmail.com>
2023-02-18 09:07:08 -0800

    6-print_numberz

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -3,12 +3,18 @@
 
 /**
 * main - Entry point
-* Description - prints all single digit numbers of base 10 starting from 0
+* Description - prints all single digit numbers of base 10.
 * Return: Always 0 (success)
 */
 
 int main(void)
 {
-	
+	int num;
+
+	for (num = 0; num < 10; num++)
+		putchar((num % 10) + '0');
+
+	putchar('\n');
+
 	return (0);
 }
