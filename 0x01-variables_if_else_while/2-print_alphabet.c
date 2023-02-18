3a89fb12d47155395e128b449944e2a199c85c72
godwinita25 <godwinita05@gmail.com>
2023-02-18 04:03:29 -0800

    1-last_digit

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+* main - Entry point
+* Description - "the program's description"
+* Return: Always 0 (success)
+*/
+
+int main(void)
+{
+
+}
