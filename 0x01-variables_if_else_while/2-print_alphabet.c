0fe8dc77eeb1acd2b2a1bdf7c59b5484a1fe5cbb
godwinita25 <godwinita05@gmail.com>
2023-02-18 04:29:59 -0800

    2-print_alphabet.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -4,11 +4,18 @@
 
 /**
 * main - Entry point
-* Description - "the program's description"
+* Description - A code that prints alphabet in lowercase
 * Return: Always 0 (success)
 */
 
 int main(void)
 {
+	char lc;
 
+	for (lc = 'a'; lc <= 'z'; lc++)
+	{
+		putchar(lc);
+	}
+	putchar('\n');
+	return (0);
 }
